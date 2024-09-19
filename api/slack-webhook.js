f397db5a64a846d86e1f40fc86304002e350bd2c
Joye <113430758+Joyeaha@users.noreply.github.com>
2024-09-19 12:35:39 +0800

    slack webhook trigger github workflow

--- api/slack-webhook.js
+++ api/slack-webhook.js
@@ -1,47 +1,63 @@
 // api/slack-webhook.js
 
-// const axios = require("axios");
-
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { challenge } = req.body;
-    console.log(req.body);
-    res.status(200).json({ challenge });
+    const { type } = req.body;
+    if (type === "url_verification") {
+      const { challenge } = req.body;
+      res.status(200).json({ challenge });
+    } else if (type === "event_callback") {
+      const { event } = req.body;
+      if (
+        event.type === "message" &&
+        (event.channel_type === "im" ||
+          event.channel === process.env.SLACK_CHANNEL_ID) &&
+        event.bot_profile?.name === "Notion" &&
+        event.text?.includes("Update Account")
+      ) {
+        triggerGithubWorkflow()
+          .then((data) => res.status(200).json(data))
+          .catch((error) => {
+            res.status(500).json({
+              error,
+            });
+          });
+      } else {
+        res.status(200).json({
+          message: "No action triggered",
+        });
+      }
+    } else {
+      res.status(404).end();
+    }
+  } else {
+    res.status(404).end();
   }
-  //   if (req.method === "POST") {
-  //     const { event } = req.body;
+}
 
-  //     // 根据 Slack 消息中的内容来判断是否触发 GitHub Action
-  //     if (event && event.text && event.text.includes("trigger action")) {
-  //       try {
-  //         // 调用 GitHub API 触发 GitHub Action
-  //         const githubResponse = await axios.post(
-  //           "https://api.github.com/repos/Joyeaha/NotionCryptoFinanceAutomation/actions/workflows/118112199/dispatches",
-  //           {
-  //             ref: "main", // 你想触发的分支
-  //           },
-  //           {
-  //             headers: {
-  //               Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-  //               Accept: "application/vnd.github.v3+json",
-  //               "X-GitHub-Api-Version": "2022-11-28",
-  //             },
-  //           }
-  //         );
+async function triggerGithubWorkflow() {
+  // 调用 GitHub API 触发 GitHub Action
+  const githubResponse = await fetch(
+    "https://api.github.com/repos/Joyeaha/NotionCryptoFinanceAutomation/actions/workflows/118112199/dispatches",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+        Accept: "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      body: JSON.stringify({
+        ref: "main",
+      }),
+    }
+  );
 
-  //         res.status(200).json({
-  //           message: "GitHub Action triggered",
-  //           githubResponse: githubResponse.data,
-  //         });
-  //       } catch (error) {
-  //         console.error("Error triggering GitHub Action:", error);
-  //         res.status(500).json({ error: "Failed to trigger GitHub Action" });
-  //       }
-  //     } else {
-  //       res.status(200).json({ message: "No action triggered" });
-  //     }
-  //   } else {
-  //     res.setHeader("Allow", ["POST"]);
-  //     res.status(405).end(`Method ${req.method} Not Allowed`);
-  //   }
+  if (!githubResponse.ok) {
+    throw new Error(
+      `GitHub API responded with status ${githubResponse.status}`
+    );
+  }
+  return {
+    message: "GitHub Action triggered",
+  };
 }
